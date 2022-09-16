193faf583fa83125102f6a0002e567dcd369cffa
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-09-16 20:27:19 -0300

    deleted unused module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { HomeSidebarComponent } from './comps/home-sidebar/home-sidebar.component';
 import { HomeHighlightsComponent } from './comps/home-highlights/home-highlights.component';
 import { HomeShelfComponent } from './comps/home-shelf/home-shelf.component';
 import { HomePlatformsComponent } from './comps/home-platforms/home-platforms.component';
@@ -10,7 +9,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     HomeComponent,
-    HomeSidebarComponent,
     HomeHighlightsComponent,
     HomeShelfComponent,
     HomePlatformsComponent,
