e330664bf30f83a3937d76dcb8563a3c81269463
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-09-28 17:25:02 -0300

    shelf click scroll

--- src/app/modules/home/comps/home-shelf/home-shelf.component.ts
+++ src/app/modules/home/comps/home-shelf/home-shelf.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-home-shelf',
@@ -10,11 +11,12 @@ export class HomeShelfComponent implements OnInit {
   @Input() items: any;
   @Input() platform?: string;
   @Input() title?: string;
+  public addressSearch!: HTMLElement;
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.shuffleArray(this.items);
+    // this.shuffleArray(this.items);
   }
 
   public letsGame(id: string) {
@@ -26,5 +28,12 @@ export class HomeShelfComponent implements OnInit {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
+  }
+
+  public scrollShelfLeft(element: HTMLElement) {
+    element.scrollTo(element.scrollLeft - element.offsetWidth, 0);
+  }
+  public scrollShelfRight(element: HTMLElement) {
+    element.scrollTo(element.scrollLeft + element.offsetWidth, 0);
   }
 }
