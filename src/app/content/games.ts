9216f00aeca87fb090c38fdb80cf094c77e3a79d
brunobozz <brunobozz@gmail.com>
2022-10-14 15:19:24 -0300

    arcade

--- src/app/content/games.ts
+++ src/app/content/games.ts
@@ -1636,7 +1636,7 @@ export const games = [
   },
   {
     title: 'Neo Bomberman',
-    platform: 'psx',
+    platform: 'arcade',
     cover: 'https://static.wikia.nocookie.net/bomberman/images/0/06/Neo_Bomberman_capa.jpg/revision/latest?cb=20200131213731&path-prefix=pt-br',
     room: 'https://www.retrogames.cc/embed/9293-neo-bomberman.html',
     id: 'd93jf9k',
